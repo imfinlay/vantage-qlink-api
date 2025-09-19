31433cf58086bb0eddbe5e4145357304ee06ded0
Ian Finlay <imfinlay@yahoo.co.uk>
2025-09-19 07:37:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
+// config.js
 module.exports = {
-    LOG_FILE_PATH: '/var/log/vantage-qlink-api.log',
-    servers: [
-        { name: 'Server1', host: '127.0.0.1', port: 4000 },
-        { name: 'Vantage', host: '10.101.111.70', port: 3040 },
-        // Add more servers as needed
-    ],
-};
+  LOG_FILE_PATH: require('path').join(__dirname, 'logs', 'http-to-tcp.log'),
 
+  // 👇 This is the list the UI/endpoint uses
+  servers: [
+    { name: 'Vantage', host: '10.101.111.70', port: 3040 },
+  ],
+
+  // optional
+  HANDSHAKE: 'VCL 1 0\r\n'
+};
