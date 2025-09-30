6cd9593eec9d4806ed745bf7736f8ab05ffca3de
Ian Finlay <imfinlay@yahoo.co.uk>
2025-09-30 11:40:06 -0400

    Debug enhancements

--- config.js
+++ config.js
@@ -8,5 +8,9 @@ module.exports = {
   ],
 
   // optional
-  HANDSHAKE: 'VCL 1 0\r\n'
+  HANDSHAKE: 'VCL 1 0\r\n',
+  debug: {
+    push: false   // set to true to enable verbose push logs
+  }
+  
 };
